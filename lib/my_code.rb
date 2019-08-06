036b697e16c0adfee57a073de0a248b88760712e
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-06 13:21:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,25 +1,59 @@
  def map_to_negativize(source_array)
-   source_array.map { |n| n * -1 }
- end 
+   n = 0 
+  new_array = []
+  while n < source_array.length 
+ new_array.push(source_array[n] * -1)
+  n += 1 
+end 
+new_array 
+end 
  
  def map_to_no_change(source_array)
-    source_array
-  end
+ n = 0 
+  new_array = []
+  while n < source_array.length 
+ new_array.push(source_array[n])
+  n += 1 
+end 
+new_array 
+end 
+
+def map_to_double(source_array) 
   
-  def map_to_double(source_array)
-    source_array.map { |n| n * 2 }
-  end 
-  
-  def map_to_square(source_array)
-    source_array.map { |n| n ** 2 }
-  end 
-  
-  def reduce_to_total(source_array)
-    source_array.reduce(:+)
-  end 
-def reduce_to_total(source_array, starting_point)
-array.reduce(starting_point) { 
-sum, num
- sum + num }
+n = 0 
+  new_array = []
+  while n < source_array.length 
+ new_array.push(source_array[n] * 2)
+  n += 1 
+end 
+new_array 
+end 
+ 
+ def map_to_square(source_array)
+   n = 0 
+  new_array = []
+  while n < source_array.length 
+ new_array.push(source_array[n] **2)
+  n += 1 
+end 
+new_array 
+end 
+
+def reduce_to_total(source_array,starting_point=0)
+   n=0 
+  total = 0
+  while n < source_array.length 
+  total += source_array[n] 
+  n+=1
+end 
+total + starting_point
+
+end
+
+def reduce_to_all_true(source_array)
+n=0 
+while n < source_array.length 
+true 
+end
 end
 
